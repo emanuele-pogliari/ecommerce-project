461f02abb7ab78b4f76cbc4d336476ac7d74f1fb
Emanuele <manu.pogliari@gmail.com>
2025-03-08 10:53:05 +0100

    migrations created (part 1)

---
+++ backend/database/migrations/2025_03_08_091849_create_product_variants_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('type_value');
+            $table->integer('stock');
+            $table->decimal('price', total: 10, places: 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variants');
+    }
+};
